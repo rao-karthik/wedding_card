e1d9cada35f20a408223c1361fcf805215e66380
Kartik Yadav <kartikyadav@Kartiks-MacBook-Pro.local>
2022-07-28 03:02:51 +0530

    improvements

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -11,42 +11,47 @@ const Header = () => {
   };
 
   return (
-    <div className='header_container'>
-      <nav className='nav_container'>
-        <div className='menu_container' onClick={handleShowOptions}>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            className='menu'
-            fill='none'
-            viewBox='0 0 24 24'
-            stroke='currentColor'
-            strokeWidth={2}>
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              d='M4 6h16M4 12h16M4 18h16'
-            />
-          </svg>
+    <>
+      {showOptions && (
+        <div className='modal_container' onClick={handleShowOptions}></div>
+      )}
+      <div className='header_container'>
+        <nav className='nav_container'>
+          <div className='menu_container' onClick={handleShowOptions}>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='menu'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+              strokeWidth={2}>
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M4 6h16M4 12h16M4 18h16'
+              />
+            </svg>
+          </div>
+        </nav>
+        <div
+          className={`header_options ${
+            showOptions ? 'show_options' : 'hide_options'
+          }`}>
+          <Link to='/' onClick={handleShowOptions}>
+            Home
+          </Link>
+          <Link to='/lagan' onClick={handleShowOptions}>
+            Lagan <span>(01-12-2022)</span>
+          </Link>
+          <Link to='/sangeet' onClick={handleShowOptions}>
+            Sangeet <span>(02-12-2022)</span>
+          </Link>
+          <Link to='/baraat' onClick={handleShowOptions}>
+            Baraat <span>(03-12-2022)</span>
+          </Link>
         </div>
-      </nav>
-      <div
-        className={`header_options ${
-          showOptions ? 'show_options' : 'hide_options'
-        }`}>
-        <Link to='/' onClick={handleShowOptions}>
-          Home
-        </Link>
-        <Link to='/lagan' onClick={handleShowOptions}>
-          Lagan <span>(01-12-2022)</span>
-        </Link>
-        <Link to='/sangeet' onClick={handleShowOptions}>
-          Sangeet <span>(02-12-2022)</span>
-        </Link>
-        <Link to='/baraat' onClick={handleShowOptions}>
-          Baraat <span>(03-12-2022)</span>
-        </Link>
       </div>
-    </div>
+    </>
   );
 };
 
